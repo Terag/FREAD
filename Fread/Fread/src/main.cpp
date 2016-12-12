d0be48e34017c72a814843b50c8ee54976422312
Terag <git@rouquette.me>
2016-12-12 16:15:24 +0100

    FColor class added to manage color

--- Fread/Fread/src/main.cpp
+++ Fread/Fread/src/main.cpp
@@ -1,24 +1,47 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include "Reader_MainTrace.hpp"
+#include "FColor.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+    /*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+    while (window.isOpen())
+    {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                    if (event.type == sf::Event::Closed)
+                            window.close();
+            }
 
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
-
-	return 0;
+            window.clear();
+            window.draw(shape);
+            window.display();
+    }*/
+    if(argc != 2){
+        std::cout << "invalid format\n valid format : Fread trace_path" << std::endl;
+        return -1;
+    }
+    
+    std::string path = argv[1];
+            
+    if(path.substr(path.size()-6, 6) != ".trace"){
+        std::cout << "invalid trace format, need *.trace" << std::endl;
+        return -1;
+    }
+    
+    //Reader_MainTrace mainTrace(path);
+    
+    FColor color(16,50,88,120);
+    std::cout << color.g << std::endl;
+    FColor Ocolor(45, 220, 90, 150);
+    std::cout << Ocolor.g << std::endl;
+    color += Ocolor;
+    std::cout << color.g << std::endl;
+    return 0;
 }
