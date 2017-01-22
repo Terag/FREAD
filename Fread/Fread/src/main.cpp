474ab3b6ca1f47b72231c82d7f463e965f77d35c
Terag <victor.rouquette@orange.fr>
2017-01-23 00:06:11 +0100

    unused lines removed from main.cpp

--- Fread/Fread/src/main.cpp
+++ Fread/Fread/src/main.cpp
@@ -8,23 +8,6 @@
 
 int main(int argc, char* argv[])
 {
-    /*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                    if (event.type == sf::Event::Closed)
-                            window.close();
-            }
-
-            window.clear();
-            window.draw(shape);
-            window.display();
-    }*/
     if(argc != 2){
         std::cout << "invalid format\n valid format : Fread trace_path" << std::endl;
         return -1;
