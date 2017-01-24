d38fca9e0d83d1d8cb329a6e60b8586b7fcf1a48
Guillem-Maxime <maxime.guillem@telecom-sudparis.eu>
2017-01-24 15:00:54 +0100

    added fqueue

---
+++ Fread/Core/Fread_Core/message_handler.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   message_handler.cpp
+ * Author: guillem
+ * 
+ * Created on 24 janvier 2017, 14:53
+ */
+
+#include "message_handler.h"
+
+message_handler::message_handler() {
+}
+
+message_handler::message_handler(const message_handler& orig) {
+}
+
+message_handler::~message_handler() {
+}
+
