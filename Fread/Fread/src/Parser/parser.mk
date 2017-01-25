7d1fa5e028955d7456211ea1d4f93c5163da663c
Terag <victor.rouquette@orange.fr>
2017-01-25 18:42:23 +0100

    makefile modified, compilation works with Shared Objects now

--- Fread/Fread/src/Parser/parser.mk
+++ Fread/Fread/src/Parser/parser.mk
@@ -1,11 +1,11 @@
 SRC = $(wildcard *.cpp)
 OBJ = $(SRC:.cpp=.o)
 
-parser.a: $(OBJ)
-	@ar rcs $@ $(OBJ)
-	@echo "    	generated	parser.a 	File"
-	@mv parser.a $(OBJ_DIR)/
-	@echo "    	parser.a move to $(OBJ_DIR)"
+parser.so: $(OBJ)
+	@g++ -o $@ -shared -fPIC $(OBJ)
+	@echo "    	generated	parser.so 	File"
+	@mv parser.so $(OBJ_DIR)/
+	@echo "    	parser.so move to $(OBJ_DIR)"
 
 statesConfig.o: statesConfig.cpp
 	@$(CXX) $(CXXFLAGS) $(INCLUDE_DIR) -o $@ -c $^
