f5d287f3295edcab117f182b9a2548364797664b
Guillem-Maxime <maxime.guillem@telecom-sudparis.eu>
2017-01-28 17:28:44 +0100

    renaming of occurrence.cpp, container.cpp and pattern.cpp to match the headers

---
+++ Fread/Fread/src/FPattern.cpp
@@ -0,0 +1,23 @@
+/*#include "FPattern.hpp"
+
+
+
+FPattern::FPattern()
+{
+}
+
+
+FPattern::~FPattern()
+{
+}
+
+bool operator==(FPattern A, FPattern B)
+{
+	return false;
+}
+
+bool operator!=(FPattern A, FPattern B)
+{
+	return false;
+}
+*/
