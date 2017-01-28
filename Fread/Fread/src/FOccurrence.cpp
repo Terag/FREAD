59615aba78a7a3eee264efb52b5a83f1e491bf80
Guillem-Maxime <maxime.guillem@telecom-sudparis.eu>
2017-01-28 17:28:44 +0100

    renaming of occurrence.cpp, container.cpp and pattern.cpp to match the headers

---
+++ Fread/Fread/src/FOccurrence.cpp
@@ -0,0 +1,21 @@
+/*#include "FOccurrence.hpp"
+
+
+
+FOccurrence::FOccurrence()
+{
+}
+
+
+FOccurrence::~FOccurrence()
+{
+}
+bool operator==(FOccurrence A, FOccurrence B)
+{
+	return false;
+}
+
+bool operator!=(FOccurrence A, FOccurrence B)
+{
+	return false;
+}*/
